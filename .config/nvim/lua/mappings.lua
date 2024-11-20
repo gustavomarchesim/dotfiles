f8c6858d72652e19ff35a43e16b79b624bcdba3f
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-11-19 22:47:01 -0300

    Update nvim config

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+map("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+map("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+map("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+map("n", "<C-\\>", ":TmuxNavigatePrevious<CR>", { silent = true })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
