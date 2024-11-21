e70bff75505d0488c9ef5a7add4fa583fd29982a
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-11-20 21:21:47 -0300

    refactor: plugins refactor

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,12 +1,9 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
 map("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
 map("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
@@ -15,5 +12,3 @@ map("n", "<C-\\>", ":TmuxNavigatePrevious<CR>", { silent = true })
 map("n", "<leader>lt", function()
   require("live-server-nvim").toggle()
 end, { desc = "Toggle Live Server" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
