ac6e1bc8dc0874082ccfac069304498a710811bc
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-11-28 22:56:12 -0300

    Update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,8 +3,17 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local unmap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+unmap("n", "<c-h>")
+unmap("n", "<c-j>")
+unmap("n", "<c-k>")
+unmap("n", "<c-l>")
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
