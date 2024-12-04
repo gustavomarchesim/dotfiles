5a9040b6e2bcd61c280a4ebdcff69d5d7e0e3985
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-12-03 21:00:17 -0300

    chore(nvim): change markdown dependencies

--- .config/nvim/lua/grimmvim/plugins/markdown.lua
+++ .config/nvim/lua/grimmvim/plugins/markdown.lua
@@ -1,9 +1,20 @@
 return {
 	{
-		"MeanderingProgrammer/markdown.nvim",
-		main = "render-markdown",
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
 		opts = {},
-		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you use the mini.nvim suite
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
 }
