08b79f40ff539b2fb42840aca8c62eda899ff166
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-12-08 16:47:13 -0300

    feat(nvim): add bufferline.nvim

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				numbers = "none",
+				diagnostics = "nvim_lsp",
+				indicator = { style = "underline" },
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+					},
+				},
+				color_icons = true,
+			},
+		})
+	end,
+}
