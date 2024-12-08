830ec3cc87410718ae4737f012c973d93a0c4440
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-12-08 16:46:02 -0300

    feat(nvim): add new autocmds

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_autocmd("BufEnter", {
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufEnter", {
 	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
 	pattern = "NvimTree_*",
 	callback = function()
@@ -9,6 +11,32 @@ vim.api.nvim_create_autocmd("BufEnter", {
 			and layout[3] == nil
 		then
 			vim.cmd("confirm quit")
+		end
+	end,
+})
+
+-- user event that loads after UIEnter + only if file buf is there
+autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
+	group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+	callback = function(args)
+		local file = vim.api.nvim_buf_get_name(args.buf)
+		local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+
+		if not vim.g.ui_entered and args.event == "UIEnter" then
+			vim.g.ui_entered = true
+		end
+
+		if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+			vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+			vim.api.nvim_del_augroup_by_name("NvFilePost")
+
+			vim.schedule(function()
+				vim.api.nvim_exec_autocmds("FileType", {})
+
+				if vim.g.editorconfig then
+					require("editorconfig").config(args.buf)
+				end
+			end)
 		end
 	end,
 })
