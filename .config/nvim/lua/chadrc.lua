e42a0be08c08ae5c0fac32b49ef00b716647eff7
Gustavo Marchesim <gustavomarchesim@outlook.com>
2025-06-11 22:59:04 -0300

    feat(nvim): New nvim config (nvchad)

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,19 +6,19 @@
 local M = {}
 
 M.base46 = {
-	theme = "yoru",
+	theme = "flexoki",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
 
 return M
--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,4 +1,3 @@
--- nvim v0.8.0
 return {
     "kdheepak/lazygit.nvim",
     lazy = true,
@@ -16,6 +15,6 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
 }
